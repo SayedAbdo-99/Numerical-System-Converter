d1cb253ed01c300fc6b690418a0bc4d6b8c745ae
Sayed_Abd_Elaleem_Sayed <30577812+SayedAbdElaleem@users.noreply.github.com>
2017-08-24 12:21:27 -0700

    Decimal_to_Bainary_And_opposite
    Convert Decimal_to_Bainary_And_opposite

---
+++ Decimal_to_Bainary_And_opposite/Decimal_to_Bainary_And_opposite/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Decimal_to_Bainary_And_opposite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Btn_Clear = new System.Windows.Forms.Button();
+            this.Btn_Exit = new System.Windows.Forms.Button();
+            this.Lab_B = new System.Windows.Forms.Label();
+            this.Lab_d = new System.Windows.Forms.Label();
+            this.Txt_Binary = new System.Windows.Forms.TextBox();
+            this.Txt_Decimal = new System.Windows.Forms.TextBox();
+            this.Btn_B_to_D = new System.Windows.Forms.Button();
+            this.Btn_D_to_B = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Decimal_to_Bainary_And_opposite.Properties.Resources.Convert_HEX_to_Decimal_in_under_30_seconds_;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(915, 598);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Btn_Clear
+            // 
+            this.Btn_Clear.BackColor = System.Drawing.Color.Teal;
+            this.Btn_Clear.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Clear.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Clear.Location = new System.Drawing.Point(506, 427);
+            this.Btn_Clear.Name = "Btn_Clear";
+            this.Btn_Clear.Size = new System.Drawing.Size(146, 48);
+            this.Btn_Clear.TabIndex = 19;
+            this.Btn_Clear.Text = "Clear";
+            this.Btn_Clear.UseVisualStyleBackColor = false;
+            this.Btn_Clear.Click += new System.EventHandler(this.Btn_Clear_Click);
+            // 
+            // Btn_Exit
+            // 
+            this.Btn_Exit.BackColor = System.Drawing.Color.Teal;
+            this.Btn_Exit.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Exit.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Exit.Location = new System.Drawing.Point(691, 427);
+            this.Btn_Exit.Name = "Btn_Exit";
+            this.Btn_Exit.Size = new System.Drawing.Size(120, 48);
+            this.Btn_Exit.TabIndex = 18;
+            this.Btn_Exit.Text = "Exit";
+            this.Btn_Exit.UseVisualStyleBackColor = false;
+            this.Btn_Exit.Click += new System.EventHandler(this.Btn_Exit_Click);
+            // 
+            // Lab_B
+            // 
+            this.Lab_B.AutoSize = true;
+            this.Lab_B.BackColor = System.Drawing.Color.Black;
+            this.Lab_B.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lab_B.ForeColor = System.Drawing.Color.Teal;
+            this.Lab_B.Location = new System.Drawing.Point(12, 154);
+            this.Lab_B.Name = "Lab_B";
+            this.Lab_B.Size = new System.Drawing.Size(240, 40);
+            this.Lab_B.TabIndex = 17;
+            this.Lab_B.Text = "Binary            :";
+            // 
+            // Lab_d
+            // 
+            this.Lab_d.AutoSize = true;
+            this.Lab_d.BackColor = System.Drawing.Color.Black;
+            this.Lab_d.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lab_d.ForeColor = System.Drawing.Color.Teal;
+            this.Lab_d.Location = new System.Drawing.Point(13, 77);
+            this.Lab_d.Name = "Lab_d";
+            this.Lab_d.Size = new System.Drawing.Size(239, 40);
+            this.Lab_d.TabIndex = 16;
+            this.Lab_d.Text = "Decimal          :";
+            // 
+            // Txt_Binary
+            // 
+            this.Txt_Binary.BackColor = System.Drawing.Color.White;
+            this.Txt_Binary.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Binary.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Txt_Binary.Location = new System.Drawing.Point(258, 154);
+            this.Txt_Binary.Name = "Txt_Binary";
+            this.Txt_Binary.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Txt_Binary.Size = new System.Drawing.Size(496, 39);
+            this.Txt_Binary.TabIndex = 15;
+            // 
+            // Txt_Decimal
+            // 
+            this.Txt_Decimal.BackColor = System.Drawing.Color.White;
+            this.Txt_Decimal.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Decimal.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Txt_Decimal.Location = new System.Drawing.Point(258, 78);
+            this.Txt_Decimal.Name = "Txt_Decimal";
+            this.Txt_Decimal.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Txt_Decimal.Size = new System.Drawing.Size(496, 39);
+            this.Txt_Decimal.TabIndex = 14;
+            // 
+            // Btn_B_to_D
+            // 
+            this.Btn_B_to_D.BackColor = System.Drawing.Color.Teal;
+            this.Btn_B_to_D.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_B_to_D.ForeColor = System.Drawing.Color.Black;
+            this.Btn_B_to_D.Location = new System.Drawing.Point(280, 427);
+            this.Btn_B_to_D.Name = "Btn_B_to_D";
+            this.Btn_B_to_D.Size = new System.Drawing.Size(146, 48);
+            this.Btn_B_to_D.TabIndex = 20;
+            this.Btn_B_to_D.Text = "B to D";
+            this.Btn_B_to_D.UseVisualStyleBackColor = false;
+            this.Btn_B_to_D.Click += new System.EventHandler(this.Btn_B_to_D_Click);
+            // 
+            // Btn_D_to_B
+            // 
+            this.Btn_D_to_B.BackColor = System.Drawing.Color.Teal;
+            this.Btn_D_to_B.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_D_to_B.ForeColor = System.Drawing.Color.Black;
+            this.Btn_D_to_B.Location = new System.Drawing.Point(106, 427);
+            this.Btn_D_to_B.Name = "Btn_D_to_B";
+            this.Btn_D_to_B.Size = new System.Drawing.Size(146, 48);
+            this.Btn_D_to_B.TabIndex = 21;
+            this.Btn_D_to_B.Text = "D to B";
+            this.Btn_D_to_B.UseVisualStyleBackColor = false;
+            this.Btn_D_to_B.Click += new System.EventHandler(this.Btn_D_to_B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 540);
+            this.Controls.Add(this.Btn_D_to_B);
+            this.Controls.Add(this.Btn_B_to_D);
+            this.Controls.Add(this.Btn_Clear);
+            this.Controls.Add(this.Btn_Exit);
+            this.Controls.Add(this.Lab_B);
+            this.Controls.Add(this.Lab_d);
+            this.Controls.Add(this.Txt_Binary);
+            this.Controls.Add(this.Txt_Decimal);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Btn_Clear;
+        private System.Windows.Forms.Button Btn_Exit;
+        private System.Windows.Forms.Label Lab_B;
+        private System.Windows.Forms.Label Lab_d;
+        private System.Windows.Forms.TextBox Txt_Binary;
+        private System.Windows.Forms.TextBox Txt_Decimal;
+        private System.Windows.Forms.Button Btn_B_to_D;
+        private System.Windows.Forms.Button Btn_D_to_B;
+    }
+}
+
---
+++ Decimal_to_Bainary_And_opposite/Decimal_to_Bainary_And_opposite/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decimal_to_Bainary_And_opposite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Reverse(string Rev)
+        {
+            char[] arr = Rev.ToCharArray();
+            Array.Reverse(arr);
+            string x=string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                x+= arr[i];
+            }
+            return x;
+        }
+        
+        private void Btn_D_to_B_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Txt_Decimal.ReadOnly = false;
+                Txt_Binary.ReadOnly = true;
+                Txt_Decimal.BackColor = Color.White;
+                Txt_Binary.BackColor = Color.DarkGray;
+                if (Txt_Decimal.Text != "")
+                {
+                   ulong val_Decimal = Convert.ToUInt64(Txt_Decimal.Text);
+                    //convert from Decimal to Bainary
+                    string val_Binary = string.Empty;
+                    while (val_Decimal > 0)
+                    {
+                        val_Binary += (val_Decimal % 2).ToString();
+                        val_Decimal /= 2;
+                    }
+                    // val_Binary=Reverse(val_Binary);
+                    Txt_Binary.Text = Reverse(val_Binary);
+
+                }
+                else if (Txt_Decimal.Text == "")
+                {
+                    Txt_Decimal.Text = "";
+                    Txt_Binary.Text = "";
+                }
+            }
+            catch (FormatException)
+            {
+                Txt_Decimal.BackColor = Color.Red;
+                MessageBox.Show("Sorry,Can not Enter flouting Number or Char or String!!!\nPlease Enter Only unsigned intger or unsigned long Numbers!");
+            }
+            catch (OverflowException)
+            {
+                Txt_Decimal.BackColor = Color.Yellow;
+                MessageBox.Show("You Must by Enter Number less then 9999999999999999999 .");
+            }
+            catch (Exception f)
+            {
+                Txt_Decimal.BackColor = Color.Green;
+                MessageBox.Show("Error!!!!\n" + f.Message);
+            }
+        }
+
+        private void Btn_B_to_D_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Txt_Decimal.ReadOnly = true;
+                Txt_Binary.ReadOnly = false;
+                Txt_Binary.BackColor = Color.White;
+                Txt_Decimal.BackColor = Color.DarkGray;
+                ulong Val_Binary = 0;
+                Txt_Binary.Text = Reverse(Txt_Binary.Text);
+                if (Txt_Binary.Text != "")
+                {
+                    for (int i = 0; i < Txt_Binary.Text.Length; i++)
+                    {
+                        if (Txt_Binary.Text[i] != '1' && Txt_Binary.Text[i] != '0')
+                            throw new FormatException("Sorry!!,Can not Eneter Any Number Other 1,0.\nPlease Enter Only Zeros And Ones..");
+                        if (Txt_Binary.Text[i] == '1')
+                            if (i == 0) Val_Binary += 1;
+                            else
+                                Val_Binary += Convert.ToUInt64(Math.Pow(2, i));
+                    }
+                    Txt_Decimal.Text = Val_Binary.ToString();
+                    Txt_Binary.Text = Reverse(Txt_Binary.Text);
+                }
+                else if (Txt_Binary.Text == "")
+                {
+                    Txt_Decimal.Text = "";
+                    Txt_Binary.Text = "";
+                }
+            }
+            catch (FormatException f) //throw Exception
+            {
+                Txt_Binary.BackColor = Color.Red;
+                MessageBox.Show(f.Message);
+            }
+            catch (OverflowException o)
+            {
+                Txt_Binary.BackColor = Color.Yellow;
+                MessageBox.Show("Sorry!!, You Entered very Large Number\n" + o.Message);
+            }
+            catch (Exception uoKnow)
+            {
+                Txt_Binary.BackColor = Color.Green;
+                MessageBox.Show("Error!!!\n"+uoKnow.Message);
+            }
+        }
+
+        private void Btn_Clear_Click(object sender, EventArgs e)
+        {
+            Txt_Decimal.Text = String.Empty;
+            Txt_Binary.Text = String.Empty;
+        }
+
+        private void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
